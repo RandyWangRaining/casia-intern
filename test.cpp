cc1915fd4c563ee62f5a9d0d95b722b5d8e18a77
wrl2003 <2835177741@qq>
2024-08-20 10:19:10 +0800

    添加了轨迹规划

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+    int x = -1;
+
+    if (x) {
+        std::cout << "-1 is true" << std::endl;
+    } 
+    else {
+        std::cout << "-1 is false" << std::endl;
+    }
+
+    if (x == false) {
+        std::cout << "-1 equals false" << std::endl;
+    } 
+    else {
+        std::cout << "-1 does not equal false" << std::endl;
+    }
+
+    return 0;
+}
